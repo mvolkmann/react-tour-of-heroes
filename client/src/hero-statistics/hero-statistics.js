5596501addd7521f09dfac71e8c9c2fd5abb3fe9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-07-07 12:23:19 -0500

    changed HeroStatistics to a function component

--- client/src/hero-statistics/hero-statistics.js
+++ client/src/hero-statistics/hero-statistics.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, {Component} from 'react';
+import React from 'react';
 import {watch} from 'redux-easy';
 
 import {type HeroMapType, heroMapToList} from '../types';
@@ -26,23 +26,21 @@ function getShortestName(nameList) {
   );
 }
 
-class HeroStatistics extends Component<PropsType> {
-  render() {
-    const nameList = heroMapToList(this.props.heroes).map(hero => hero.name);
-    return (
-      <div className="hero-statistics">
-        <div>
-          <label># of heroes:</label> {nameList.length}
-        </div>
-        <div>
-          <label>Shortest Name:</label> {getShortestName(nameList)}
-        </div>
-        <div>
-          <label>Longest Name:</label> {getLongestName(nameList)}
-        </div>
+const HeroStatistics = (props: PropsType) => {
+  const nameList = heroMapToList(props.heroes).map(hero => hero.name);
+  return (
+    <div className="hero-statistics">
+      <div>
+        <label># of heroes:</label> {nameList.length}
       </div>
-    );
-  }
-}
+      <div>
+        <label>Shortest Name:</label> {getShortestName(nameList)}
+      </div>
+      <div>
+        <label>Longest Name:</label> {getLongestName(nameList)}
+      </div>
+    </div>
+  );
+};
 
 export default watch(HeroStatistics, {heroes: ''});
